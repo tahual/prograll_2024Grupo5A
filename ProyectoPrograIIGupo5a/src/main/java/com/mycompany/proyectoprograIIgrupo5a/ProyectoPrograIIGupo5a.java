f91946c90f21f81ccdf690510ca19aa8e445268f
tahual <dtahalc@miumg.edu.gt>
2024-09-26 23:25:04 -0600

    creacion del repocitorio archivo del proyecto

---
+++ ProyectoPrograIIGupo5a/src/main/java/com/mycompany/proyectoprograIIgrupo5a/ProyectoPrograIIGupo5a.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.proyectoprograIIgrupo5a;
+
+/**
+ *
+ * @author Dany Alexsis Tahual
+ */
+public class ProyectoPrograIIGupo5a {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
